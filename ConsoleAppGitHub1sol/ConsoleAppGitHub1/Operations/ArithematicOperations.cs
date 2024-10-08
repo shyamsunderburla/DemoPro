270920774600cb1c6f5170a710a6229c1a29ffcb
shyamsunderburla <shyamsunderburla2000.vtalent@gmail.com>
2024-10-08 18:35:44 +0530

    implementated code changes for arithematic Operations

---
+++ ConsoleAppGitHub1sol/ConsoleAppGitHub1/Operations/ArithematicOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGitHub1.Operations
+{
+    public class ArithematicOperations
+    {
+       public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
--- ConsoleAppGitHub1sol/ConsoleAppGitHub1/Program.cs
+++ ConsoleAppGitHub1sol/ConsoleAppGitHub1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleAppGitHub1.Operations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,20 @@ namespace ConsoleAppGitHub1
     {
         static void Main(string[] args)
         {int a,b,c;
-         a=10;
-         b=20;
-         c=a+b;
+            Console.WriteLine("enter a value :");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter b value :");
+            b = Convert.ToInt32(Console.ReadLine());
+
+           ArithematicOperations obj = new ArithematicOperations();
+         c=obj.Add(a,b);
          Console.WriteLine("the sum is ."+c);
-          Console.WriteLine(" Program Completed");
+
+            c = obj.sub(a, b);
+            Console.WriteLine("the sub is ." + c);
+            Console.WriteLine(" Program Completed");
+            Console.ReadKey();
         }
     }
 }
